35f6764923ca743d6dadbc7b96b9b9452723841a
Szczepan Rydzewski <srydzewski@inf19.local>
2023-09-14 14:59:41 +0200

    l1

--- index.php
+++ index.php
@@ -5,14 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    
 </head>
 <body>
     text
-<?php
-print("<i>italicaks</i><br>");
-echo("el1"."el2");
-    //coment
-    phpinfo();
-?>
+    <?php
+        print("<i>italicaks</i><br>");
+        echo("el1"."el2"."<br>");//konkatenacja
+        echo("<img style='height:300px' src='obrazek.jpg'>");
+        //coment
+        //phpinfo();
+        echo("<br>");
+
+        echo(true);
+        echo (false);
+        //l 8 poprzedzmy 0 
+        //l 16 poprzedzmy 0x 
+        define("x",54678);
+        echo("<br>");
+
+        echo(x);
+        echo("<br>");
+
+        $r=10;
+        echo($r);
+        echo("<br>");
+
+        echo($r==10);
+        echo("<br>");
+        echo($r==="10");
+
+        $y="10";
+        echo($y+10)
+
+        //if elseif else 
+        
+    ?>
 </body>
 </html>
