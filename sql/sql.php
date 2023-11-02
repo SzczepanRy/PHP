e5f1ceeea8fa0b96f3dabe247b5d6ef63835e25a
Szczepan Rydzewski <srydzewski@inf19.local>
2023-11-02 14:50:37 +0100

    a

--- sql/sql.php
+++ sql/sql.php
@@ -1,4 +1,9 @@
 <?php
+$id="";
+$imie ="";
+$nazwisko ="";
+$przycisk = "dodaj";
+
 mysqli_report(MYSQLI_REPORT_OFF);
 $conn=mysqli_connect("localhost","root","", "srydz");
 if(!$conn) die("brak połączenia");
@@ -11,14 +16,53 @@ if(isset($_GET['akcja']))
                 $id=$_GET["id"];
                 mysqli_query($conn,"DELETE FROM tablica WHERE id='$id'")  or die("błąd usuwania");
             }
+            break;
+        case "dodaj":
+            if(isset($_GET["imie"], $_GET["nazwisko"]))
+            {
+                $imie = $_GET['imie'];
+                $nazwisko = $_GET['nazwisko'];
+            
+                mysqli_query($conn, "INSERT INTO tablica(imie, nazwisko) VALUES('$imie','$nazwisko')") or die("insert nie dziala");
+                $imie ="";
+                $nazwisko ="";
+                $przycisk = "dodaj";
+            }
+            break;
+
+        case "edytuj":
+            if(isset($_GET["id"]))
+            {
+               
+                $id=$_GET["id"];
+              
+                $rs = mysqli_query($conn, "SELECT imie,nazwisko FROM tablica WHERE id=$id") or die("insert nie dziala");
+                $row = mysqli_fetch_array($rs);
+                $imie = $row['imie'];
+                $nazwisko = $row['nazwisko'];
+                $przycisk = "zapisz";
+            }
+            break;
+
+        case "zapisz":
+            if(isset($_GET["imie"], $_GET["nazwisko"],$_GET["id"]))
+            {
+                $id=$_GET["id"];
+                $imie = $_GET['imie'];
+                $nazwisko = $_GET['nazwisko'];
+            
+                mysqli_query($conn, "UPDATE tablica SET imie='$imie',nazwisko='$nazwisko' WHERE id=$id") or die(" nie dziala");
+              
+                $imie ="";
+                $nazwisko ="";
+                $przycisk = "dodaj";
+            }
+            break;
+
+            
     }
 }
-if(isset($_GET["imie"], $_GET["nazwisko"]))
-{
-    $imie = $_GET['imie'];
-    $nazwisko = $_GET['nazwisko'];
-    mysqli_query($conn, "INSERT INTO tablica(imie, nazwisko) VALUES('$imie','$nazwisko')") or die("insert nie dziala");
-}
+
 $result=mysqli_query($conn,"SELECT id,imie,nazwisko FROM tablica") or die("błąd w select");
 if(mysqli_num_rows($result)>0)
 {
@@ -36,11 +80,14 @@ if(mysqli_num_rows($result)>0)
 else
     echo "brak wyników";
 mysqli_close($conn);
-?>
 
+echo"
 <br>
 <form>
-    imie: <input type="text" name="imie"> <br>
-    nazwisko: <input type="text" name="nazwisko"> <br>
-    <input type="submit" value="dodaj" name='akcja'>
-</form>
+    imie: <input type='text' name='imie' value='$imie'> <br>
+    nazwisko: <input type='text' name='nazwisko' value='$nazwisko'> <br>
+    
+    <input type='hidden' value='$id' name='id'>
+    <input type='submit' value='$przycisk' name='akcja'>
+</form>"
+?>
